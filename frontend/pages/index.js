a94b93d19e795f6c413440306920ed44e56253b0
Pavel Tikhonin <p.tikhonin@mail.ru>
2025-02-20 14:35:57 +0300

    Updated homepage content

---
+++ frontend/pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <div>
+            <h1>Welcome to Dark Academia Hub 📚</h1>
+            <p>This is a place for intellectual discussions, literature, and knowledge.</p>
+        </div>
+    );
+}
+
